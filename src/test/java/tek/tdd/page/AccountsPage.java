6d2140ceb6f8d1f2a5ddeb668f1ab3e0f9a278a9
Kulvinder Kaur <kaurk1909@gmail.com>
2024-04-25 11:31:28 -0400

    Updated Changes to TDD framework

--- src/test/java/tek/tdd/page/AccountsPage.java
+++ src/test/java/tek/tdd/page/AccountsPage.java
@@ -5,15 +5,42 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import tek.tdd.utilities.SeleniumUtilities;
 
+import java.util.List;
+
 public class AccountsPage extends SeleniumUtilities {
 
     public AccountsPage() {
         PageFactory.initElements(getDriver(), this);
     }
     @FindBy(xpath = "/html/body/div[1]/div/div[2]/div/div/a[1]")
+    ////*[@id="root"]/div/div[2]/div/div/a[1]
     public WebElement accountButton;
 
-    @FindBy(xpath = "/html/body/div[1]/div/div[3]/div/div/div/div[2]/div/div/div[3]/select")
-    public WebElement accountRows;
+    @FindBy(xpath = "/html/body/div[1]/div/div[3]/div/div/div/div[2]/div/div/div[3]/select/option[1]")
+    public WebElement accountRows5;
 
+    @FindBy(xpath = "/html/body/div[1]/div/div[3]/div/div/div/div[2]/div/div/div[3]/select")
+    public WebElement accountRows10;
+
+    @FindBy(xpath = "/html/body/div[1]/div/div[3]/div/div/div/div[2]/table")
+    ///html/body/div[1]/div/div[3]/div/div/div/div[2]/table/thead
+    public List<WebElement> recordPageTen;
+    @FindBy(xpath = "/html/body/div[1]/div/div[3]/div/div/div/div[2]/div/div/div[3]/select")
+    public WebElement accountRow25;
+
+    @FindBy(xpath = "/html/body/div[1]/div/div[3]/div/div/div/div[2]/table/thead/tr")
+    public List<WebElement> recordPageTwentyFive;
+
+    @FindBy(xpath = "/html/body/div[1]/div/div[3]/div/div/div/div[2]/div/div/div[3]/select")
+    public WebElement accountRow50;
+
+    @FindBy(xpath = "/html/body/div[1]/div/div[3]/div/div/div/div[2]/table/thead/tr")
+    public List<WebElement> recordPageFifty;
+
+    public int countRecordPageTen() {return recordPageTen.size();
+    }
+    public int countRecordPageTwentyFive(){ return recordPageTwentyFive.size();
+    }
+    public int countRecordPageFifty(){ return recordPageFifty.size();
+    }
 }
--- src/test/java/tek/tdd/test/AccountTest.java
+++ src/test/java/tek/tdd/test/AccountTest.java
@@ -2,10 +2,68 @@ package tek.tdd.test;
 
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 import tek.tdd.base.BaseUITest;
 
 import java.util.List;
 
 public class AccountTest extends BaseUITest {
+    @Test
+    public void NavigateToCustomerServicePortalNavigateVerify5row() {
+        clickOnElement(loginPage.loginBtn);
+        sendText(loginPage.userName, "supervisor");
+        sendText(loginPage.userPassword, "tek_supervisor");
+        clickOnElement(loginPage.signInBtn);
+        clickOnElement(loginPage.customerServicePortalPage);
+        clickOnElement(accountsPage.accountButton);
 
+        String expectedRow = "Show 5";
+        String actualRow = getElementText(accountsPage.accountRows5);
+        Assert.assertEquals(actualRow, expectedRow, "The expected row should be same as actual row");
+    }
+
+    @Test
+    public void NavigateToCustomerServicePortalNavigateToAccountPerPageToTenValidateTableRecord() {
+        clickOnElement(loginPage.loginBtn);
+        sendText(loginPage.userName, "supervisor");
+        sendText(loginPage.userPassword, "tek_supervisor");
+        clickOnElement(loginPage.signInBtn);
+        clickOnElement(accountsPage.accountButton);
+        selectFromDropDown(accountsPage.accountRows10, "Show 10");
+        int recordCount = accountsPage.countRecordPageTen();
+        recordCount--;
+        int actualRecord=recordCount;
+        int expectedRecord = 10;
+        Assert.assertEquals(actualRecord, expectedRecord);
+    }
+
+    @Test
+    public void NavigateToCustomerServicePortalNavigateToAccountPerPageToTwentyFiveValidateTableRecord() {
+        clickOnElement(loginPage.loginBtn);
+        sendText(loginPage.userName, "supervisor");
+        sendText(loginPage.userPassword, "tek_supervisor");
+        clickOnElement(loginPage.signInBtn);
+        clickOnElement(accountsPage.accountButton);
+        selectFromDropDown(accountsPage.accountRow25, "Show 25");
+        int recordCountOne = accountsPage.countRecordPageTwentyFive();
+        recordCountOne--;
+        int actualRecordOne = recordCountOne;
+        int expectedRecordOne = 25;
+        Assert.assertEquals(actualRecordOne,expectedRecordOne);
+    }
+
+    @Test
+    public void NavigateToCustomerServicePortalNavigateToAccountPerPageToFiftyValidateTableRecord() {
+        clickOnElement(loginPage.loginBtn);
+        sendText(loginPage.userName, "supervisor");
+        sendText(loginPage.userPassword, "tek_supervisor");
+        clickOnElement(loginPage.signInBtn);
+        clickOnElement(accountsPage.accountButton);
+        selectFromDropDown(accountsPage.accountRow50, "Show 50");
+        int recordCountTwo = accountsPage.countRecordPageFifty();
+        recordCountTwo;
+        int actualRecordTwo = recordCountTwo;
+        int expectedRecordTwo = 50;
+        Assert.assertEquals(actualRecordTwo, expectedRecordTwo);
+    }
 }
